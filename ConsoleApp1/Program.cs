b6981cc22d02ea43838ba241471fb54e3f423742
kimhyeok <kimmang940715@naver.com>
2023-09-11 22:03:51 +0900

    프레임 관리 추가

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,38 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+
+            Console.CursorVisible = false;
+
+            const int WAIT_TICK = 1000 / 30;
+            // 시간 측정
+            int lastTick = 0;
+
+            while (true)
+            {
+                #region FPS
+                // FPS (프레임 60프레임 OK 30이하 NO)
+
+                // 시스템 시작 이후 경과된 ms 리턴
+                int currentTick = System.Environment.TickCount;
+
+                // 만약에 경과한 시간이 1/30초 보다 작다면 1초 = 1000ms
+                if (currentTick - lastTick < WAIT_TICK)
+                {
+                    continue;
+                }
+                lastTick = currentTick;
+                #endregion
+
+                // 입력
+
+                // 로직
+
+                // 렌더링
+                Console.WriteLine("Hello World");
+                Console.SetCursorPosition(0, 0);
+
+            }
         }
     }
 }
