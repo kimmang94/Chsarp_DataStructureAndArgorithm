1f4b4316c5ba11e372a33a15d2e5e29ebcac6302
kimhyeok <kimmang940715@naver.com>
2023-09-12 17:37:31 +0900

    배열, 동적배열, 열결리스트 추가

--- ConsoleApp1/Board.cs
+++ ConsoleApp1/Board.cs
@@ -8,5 +8,14 @@ namespace Algorithm
 {
     class Board
     {
+        public int[] _data = new int[25]; // 배열
+        public List<int> _data2 = new List<int>(); // 동적 배열 C++ 에서는 백터
+        public LinkedList<int> _data3 = new LinkedList<int>(); // (이중) 연결 리스트 C++ 에서는 리스트
+
+
+        public void Initialize()
+        {
+
+        }
     }
 }
