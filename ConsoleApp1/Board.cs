6bdfba2554c308ab82c5f7c1b516fcf48bc54054
kimhyeok <kimmang940715@naver.com>
2023-09-12 19:19:33 +0900

    연결 리스트 구현 연습

--- ConsoleApp1/Board.cs
+++ ConsoleApp1/Board.cs
@@ -51,17 +51,90 @@ namespace Algorithm
         }
 
     }
-#endregion
+    #endregion
+
+    #region 연결 리스트 구현 연습
+    class MyLinkedListNode<T>
+    {
+        public T Data;
+        public MyLinkedListNode<T> Next;
+        public MyLinkedListNode<T> Prev;
+    }
+
+    class MyLinkedList<T>
+    {
+        public MyLinkedListNode<T> Head = null; // 첫번째
+        public MyLinkedListNode<T> Tail = null; // 마지막
+        public int Count = 0;
+
+        public MyLinkedListNode<T> AddLast(T data)
+        {
+            MyLinkedListNode<T> newRoom = new MyLinkedListNode<T>();
+            newRoom.Data = data;
+
+            // 만약에 아직 방이 아예 없었다면, 새로 추가한 첫번째 방이 곧 Head 이다
+            if (Head == null)
+            {
+                Head = newRoom;
+            }
+
+            // 기존의 [마지막방] 과 [새로 추가되는 방]을 연결해준다.
+            if (Tail != null)
+            {
+                Tail.Next = newRoom;
+                newRoom.Prev = Tail;
+            }
+
+            // [새로 추가되는 방]을 [마지막 방]으로 인정한다 
+            Tail = newRoom;
+            Count++;
+            return newRoom;
+        }
+
+        public void Remove(MyLinkedListNode<T> room)
+        {
+            // [기존의 첫번째 방 다음 방] 을 [첫번째 방으로] 인정한다.
+            if (Head == room)
+            {
+                Head = Head.Next;
+            }
+
+            // [기존의 마지막 방의 이전 방] 을 [마지막 방으로] 인정한다
+            if (Tail == room)
+            {
+                Tail = Tail.Prev;
+            }
+
+            if (room.Prev != null)
+            {
+                room.Prev.Next = room.Next;
+            }
+
+            if (room.Next != null)
+            {
+                room.Next.Prev = room.Prev;
+            }
+
+            Count--;
+        }
+    }
+    #endregion
+
     class Board
     {
         public int[] _data = new int[25]; // 배열
-        public List<int> _data2 = new List<int>(); // 동적 배열 C++ 에서는 백터
         public LinkedList<int> _data3 = new LinkedList<int>(); // (이중) 연결 리스트 C++ 에서는 리스트
 
 
         public void Initialize()
         {
+            _data3.AddLast(101);
+            _data3.AddLast(102);
+            LinkedListNode<int> node =_data3.AddLast(103);
+            _data3.AddLast(104);
+            _data3.AddLast(105);
 
+            _data3.Remove(node);
         }
     }
 }
