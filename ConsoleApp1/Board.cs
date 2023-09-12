479ba6abe9812a02cebde4a78d8d4e3f2d49b854
kimhyeok <kimmang940715@naver.com>
2023-09-12 18:39:02 +0900

    연결 리스트 구현 연습을 위한 코드 삭제 및 초기화

--- ConsoleApp1/Board.cs
+++ ConsoleApp1/Board.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Algorithm
 {
-    class  MyList<T>
+    #region 동적 배열 구현 연습
+    class MyList<T>
     {
         const int DEFAUT_SIZE = 1;
         T[] _data = new T[DEFAUT_SIZE];
@@ -50,7 +51,7 @@ namespace Algorithm
         }
 
     }
-
+#endregion
     class Board
     {
         public int[] _data = new int[25]; // 배열
@@ -60,15 +61,7 @@ namespace Algorithm
 
         public void Initialize()
         {
-            _data2.Add(101);
-            _data2.Add(102);
-            _data2.Add(103);
-            _data2.Add(104);
-            _data2.Add(105);
 
-            int temp = _data2[2];
-
-            _data2.RemoveAt(2);
         }
     }
 }
